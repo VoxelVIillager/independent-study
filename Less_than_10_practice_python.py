8857c51ef69a3dc26f9e912a7f1c4ba7cdd59405
VoxelVIillager <58152675+VoxelVIillager@users.noreply.github.com>
2019-11-24 19:10:53 -0500

    Add files via upload
    This is a modification of the practicepython.org assignment less than 10.  Instead of using raw numbers, the list sorts the lengths of names of paper mario characters.

---
+++ Less_than_10_practice_python.py
@@ -0,0 +1,27 @@
+"""Nadav and Dad
+Notes on PracticePython#3: less than 10
+Authors:  Nadav Gerber, Michael Gerber
+
+Takes a list of names of paper mario characters and
+prints out the names of all all characters with 5
+letters in the name or less"""
+#what are the steps of this program
+#create a list of character names
+list_of_names = ["mario", "luigi", "peach"]
+#sort through the list.  You will need to use
+#   the for loop here.  Also length of name is len(name)
+#   find out if len(name) are bigger or less than 5
+#Print the len(name) less than 5
+print(list_of_names)
+
+#the for statement below goes through every item of
+#the list 'list_of_names'
+for i in list_of_names:
+    #add a statement here about checking len(name)
+    print len(i)
+
+#my_list = [str("mario]
+
+#for i in my_list:
+#    print(i)
+
