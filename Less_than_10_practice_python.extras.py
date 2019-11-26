1a8816673b643a3c7d26cd96a5d3188adc6c763a
VoxelVIillager <58152675+VoxelVIillager@users.noreply.github.com>
2019-11-25 19:41:00 -0500

    Add files via upload
    www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
    
    Hey.  Your job is to print all the names that are five letters or less.  Remember to append the names to the new list called five_or_less.  Then print it after the for loop, and save to github, yo.

---
+++ Less_than_10_practice_python.extras.py
@@ -0,0 +1,22 @@
+"""Nadav and Dad
+Notes on PracticePython#3: less than 10
+Authors:  Nadav Gerber, Michael Gerber
+
+Takes a list of names of paper mario characters and
+prints out the names of all all characters with 5
+letters in the name or less.
+
+This version is an extra and will print them all at once."""
+
+list_of_names = ["mario", "luigi", "defender", "galaga", "blow dryer", "punjabi", "peach", "tippi", "yoshi", "pennington", "mii", "mg", "wii"]
+five_or_less = []
+for i in list_of_names:
+    if len(i) < 5:
+        #instead of printing here, you append i to the list five_or_less
+
+
+#whatever statement you have here will run once the for loop is finished.
+#you will print the list five_or_less
+
+
+
